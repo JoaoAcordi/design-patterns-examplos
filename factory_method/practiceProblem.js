5fd0cf70eaccf772cada4fcc75dd0fe5e45dbbe7
João Pedro Acordi <88681190+JoaoAcordi@users.noreply.github.com>
2025-08-27 19:52:59 -0300

    Update practiceProblem.js

--- factory_method/practiceProblem.js
+++ factory_method/practiceProblem.js
@@ -1,28 +1,38 @@
-// Classes concretas
 class DebitCard {
   pay(type, amount) {
-    console.log(`Pagando com ${type} no valor de R$ ${amount}`);
+    console.log(`Pagando com ${type} no valor de ${amount}`);
   }
 }
 
 class CreditCard {
   pay(type, amount) {
-    console.log(`Pagando com ${type} no valor de R$ ${amount}`);
+    console.log(`Pagando com ${type} no valor de ${amount}`);
   }
 }
 
-// Código do cliente
-function main() {
-  const type = "credit card";
+class PaymentMethodFactory {
+  static types = {
+    "debit card": DebitCard,
+    "credit card": CreditCard,
+  };
 
-  let expenses;
-  if (type === "debit card") {
-    expenses = new DebitCard();
-  } else if (type === "credit card") {
-    expenses = new CreditCard();
+  static createPaymentMethod(type) {
+    const PaymentClass = this.types[type];
+    if (!PaymentClass) {
+      throw new Error("Método de pagamento não suportado");
+    }
+    return new PaymentClass();
   }
+}
 
-  expenses.pay(`${type}`, "R$ 500,00");
+function main() {
+  const type = "credit card"; 
+  try {
+    const paymentMethod = PaymentMethodFactory.createPaymentMethod(type);
+    paymentMethod.pay(type, "R$ 500,00");
+  } catch (err) {
+    console.error("Erro:", err.message);
+  }
 }
 
 main();
